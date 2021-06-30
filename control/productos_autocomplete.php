8201a9737a5a4c95dd7ab508ed3eef960638514c
ferchuse <sistemas@glifo.mx>
2021-06-30 18:41:44 -0500

    Update productos_autocomplete.php

--- control/productos_autocomplete.php
+++ control/productos_autocomplete.php
@@ -1,10 +1,12 @@
 <?php
-	
+	header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+	header("Cache-Control: post-check=0, pre-check=0", false);
+	header("Pragma: no-cache");
 	include ("../conexi.php");
 	header("Content-Type: application/json");
 	
 	$link=Conectarse();
-	
+	 
 	$respuesta  =array() ;
 	$query=$_GET["query"]; 
 	$tabla= "productos"; 
